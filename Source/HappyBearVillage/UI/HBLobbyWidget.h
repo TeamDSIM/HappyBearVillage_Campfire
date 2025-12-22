81804ca1153906c59bb8a0a7f6ac5c8fe2922a07
jsub1379 <145996022+jsub1379@users.noreply.github.com>
2025-12-22 09:56:24 +0900

    feat : Lobby UI 기본 구조 설정
    WBP_Lobby 생성
    HBLobbyWidget 생성

---
+++ Source/HappyBearVillage/UI/HBLobbyWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HBLobbyWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAPPYBEARVILLAGE_API UHBLobbyWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
