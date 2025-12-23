9b2f58b1cce389a3f2dd75613a39a3ca0673800b
oeccsy <oeccsy@gmail.com>
2025-12-23 16:18:31 +0900

    feat : PCG Village Actor

---
+++ Source/HappyBearVillage/ProceduralGeneration/Map/HBPCGVillageActor.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HBPCGVillageActor.h"
+#include "PCGComponent.h"
+#include "PCGGraph.h"
+#include "Elements/PCGTextureSampler.h"
+#include "Components/BoxComponent.h"
+
+AHBPCGVillageActor::AHBPCGVillageActor()
+{
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	PCG = CreateDefaultSubobject<UPCGComponent>(TEXT("PCGComponent"));
+	Bounds = CreateDefaultSubobject<UBoxComponent>(TEXT("PCGBounds"));
+
+	/*
+	static ConstructorHelpers::FObjectFinder<UPCGGraph> GraphAsset(TEXT(""));
+	if (GraphAsset.Succeeded())
+	{
+		PCG->SetGraph(GraphAsset.Object);
+	}
+	*/
+}
+
+void AHBPCGVillageActor::InitializePCGInput(UTexture2D* Texture2D)
+{
+	UPCGGraph* Graph = PCG->GetGraph();
+
+	for (UPCGNode* Node : Graph->GetNodes())
+	{
+		UPCGSettings* Settings = Node->GetSettings();
+		if (!Settings) continue;
+
+		if (Settings->IsA<UPCGTextureSamplerSettings>())
+		{
+			UPCGTextureSamplerSettings* TextureSettings = Cast<UPCGTextureSamplerSettings>(Settings);
+			UE_LOG(LogTemp, Log, TEXT("%s Found!"), *Settings->GetName());
+
+			TextureSettings->SetTexture(Texture2D);
+		}
+	}
+}
+
+void AHBPCGVillageActor::Generate()
+{
+	PCG->Cleanup();
+	PCG->Generate();
+}
+
+void AHBPCGVillageActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/HappyBearVillage/ProceduralGeneration/Map/HBPCGVillageActor.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HBPCGVillageActor.generated.h"
+
+UCLASS()
+class HAPPYBEARVILLAGE_API AHBPCGVillageActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AHBPCGVillageActor();
+
+	void InitializePCGInput(UTexture2D* Texture2D);
+	void Generate();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<class UStaticMeshComponent> Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<class UPCGComponent> PCG;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<class UBoxComponent> Bounds;
+};
