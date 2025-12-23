9f90d424d4a85d53ba557f5a39e6a4901fdbffc8
Hyeonyong16 <dizzyking77@naver.com>
2025-12-23 19:55:40 +0900

    feat : Log 기능 및 카테고리 추가

--- Source/HappyBearVillage/HappyBearVillage.cpp
+++ Source/HappyBearVillage/HappyBearVillage.cpp
@@ -3,4 +3,9 @@
 #include "HappyBearVillage.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(LogHY);
+DEFINE_LOG_CATEGORY(LogJS);
+DEFINE_LOG_CATEGORY(LogHW);
+DEFINE_LOG_CATEGORY(LogYS);
+
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HappyBearVillage, "HappyBearVillage" );
--- Source/HappyBearVillage/HappyBearVillage.h
+++ Source/HappyBearVillage/HappyBearVillage.h
@@ -4,3 +4,43 @@
 
 #include "CoreMinimal.h"
 
+// Local Role.
+#define LOG_SUBLOCALROLEINFO \
+*(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetOwner()->GetLocalRole()))
+
+// Remote Role.
+#define LOG_SUBREMOTEROLEINFO \
+*(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetOwner()->GetRemoteRole()))
+
+// Local Role.
+#define LOG_LOCALROLEINFO \
+*(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetLocalRole()))
+
+// Remote Role.
+#define LOG_REMOTEROLEINFO \
+*(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetRemoteRole()))
+
+// NetMode.
+#define LOG_NETMODEINFO ( (GetNetMode() == ENetMode::NM_Client) ? \
+*FString::Printf(TEXT("Client_%d"), UE::GetPlayInEditorID()) : \
+(GetNetMode() == ENetMode::NM_Standalone) ? \
+TEXT("StandAlone") : TEXT("Server") )
+
+// __FUNCTION__ -> const char* (ANSI).
+#define LOG_CALLINFO ANSI_TO_TCHAR(__FUNCTION__)
+
+#define HB_LOG(LogCat, Verbosity, Format, ...) \
+UE_LOG(LogCat, Verbosity, TEXT("[%s][%s/%s] %s %s"), \
+LOG_NETMODEINFO, LOG_LOCALROLEINFO, LOG_REMOTEROLEINFO, LOG_CALLINFO, \
+*FString::Printf(Format, ##__VA_ARGS__))
+
+#define HB_SUBLOG(LogCat, Verbosity, Format, ...) \
+UE_LOG(LogCat, Verbosity, TEXT("[%s][%s/%s] %s %s"), \
+LOG_NETMODEINFO, LOG_SUBLOCALROLEINFO, LOG_SUBREMOTEROLEINFO, LOG_CALLINFO, \
+*FString::Printf(Format, ##__VA_ARGS__))
+
+DECLARE_LOG_CATEGORY_EXTERN(LogHY, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogJS, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogHW, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogYS, Log, All);
+
