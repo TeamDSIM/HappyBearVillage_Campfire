8a7cf716838c6d254ff54f8f526fb04788917209
oeccsy <oeccsy@gmail.com>
2025-12-26 12:12:07 +0900

    feat : Utils 함수를 Seed 기반 함수로 교체

--- Source/HappyBearVillage/Utils/HBUtils.cpp
+++ Source/HappyBearVillage/Utils/HBUtils.cpp
@@ -1,1 +1,18 @@
 ﻿#include "HBUtils.h"
+
+FRandomStream HBUtils::RandomStream;
+
+void HBUtils::InitRandomSeed(int32 InSeed)
+{
+	RandomStream.Initialize(InSeed);
+}
+
+float HBUtils::GetRandomFloatFromStream(float Min, float Max)
+{
+	return RandomStream.FRandRange(Min, Max);
+}
+
+int32 HBUtils::GetRandomInt32FromStream(int32 Min, int32 Max)
+{
+	return RandomStream.RandRange(Min, Max);
+}
