9f6f20db10200db5d1dbfa637d44b3b048324b29
jsub1379 <145996022+jsub1379@users.noreply.github.com>
2026-01-07 17:41:17 +0900

    feat : GameData 파일 추가 완료
    .csv 파일 추가
    파일 읽기 위한 헤더 파일 추가
    UASSET에 데이터 테이블 추가

---
+++ Source/HappyBearVillage/JobInfo/HBJobInfo.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "HBJobInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FHBJobInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="JobInfo")
+	FString JobName = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JobInfo")
+	FString JobNameDisplay = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JobInfo")
+	FString JobInfo1 = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JobInfo")
+	FString JobInfo2 = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JobInfo")
+	FString JobInfoDetail1 = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JobInfo")
+	FString JobInfoDetail2 = TEXT("");
+};
