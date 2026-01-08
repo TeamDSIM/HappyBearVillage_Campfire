198d71404363f0b9642ddc74f77ab0789cff8a72
jsub1379 <145996022+jsub1379@users.noreply.github.com>
2026-01-08 16:14:42 +0900

    feat : 몽유병 직업 구현 완료
    맵에서 접근할 수 있는 위치 중 랜덤으로 선택하여 이동하도록 구현

--- Source/HappyBearVillage/Character/Component/Job/HBJobSleepwalkerComponent.cpp
+++ Source/HappyBearVillage/Character/Component/Job/HBJobSleepwalkerComponent.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Character/Component/Job/HBJobSleepwalkerComponent.h"
+#include "Subsystem/HBVillageGenerationWorldSubsystem.h"
+#include "Prop/HBVillage.h"
+
 
 #include "Character/HBCharacterPlayer.h"
 UHBJobSleepwalkerComponent::UHBJobSleepwalkerComponent()
@@ -14,7 +17,18 @@ void UHBJobSleepwalkerComponent::NightPhaseBegin()
 	Super::NightPhaseBegin();
 
 	//테스트용 위치
-	FVector TestLocation = FVector(0.0f, 0.0f, 1000.0f);
+	//FVector TestLocation = FVector(0.0f, 0.0f, 1000.0f);
+
+	UHBVillageGenerationWorldSubsystem* WorldSubsystem = GetWorld()->GetSubsystem< UHBVillageGenerationWorldSubsystem>();
+
+	if (!WorldSubsystem)
+	{
+		UE_LOG(LogTemp, Log, TEXT("No UHBVillageGenerationWorldSubsystem"));
+	}
+	TArray<FVector> AvailableArray = WorldSubsystem->GetVillage()->GetAvailableBlockLocations();
+
+	int32 Index = FMath::RandRange(0, AvailableArray.Num() - 1);
+	FVector RandomAvailableLocation = AvailableArray[Index];
 
 	UE_LOG(LogTemp, Log, TEXT("SleepWalkerTry"));
 	AHBCharacterPlayer* HBCharacterPlayer = GetOwner<AHBCharacterPlayer>();
@@ -22,7 +36,7 @@ void UHBJobSleepwalkerComponent::NightPhaseBegin()
 	if (HBCharacterPlayer)
 	{
 		UE_LOG(LogTemp, Log, TEXT("SleepWalkerTry TestLocate"));
-		HBCharacterPlayer->SetActorLocation(TestLocation);
+		HBCharacterPlayer->SetActorLocation(RandomAvailableLocation);
 	}
 
 
