03a3d807ec233140492c09ccc9f06a009dd88ee0
oeccsy <oeccsy@gmail.com>
2026-01-11 15:43:27 +0900

    test : spawn invitation

--- Source/HappyBearVillage/Character/Component/Job/HBJobInsiderComponent.cpp
+++ Source/HappyBearVillage/Character/Component/Job/HBJobInsiderComponent.cpp
@@ -34,38 +34,36 @@ void UHBJobInsiderComponent::Action()
 	{
 		return;
 	}
-
-	UE_LOG(LogTemp, Warning, TEXT("Insider Action"));
+	
 	SpawnInvitation();
 }
 
 void UHBJobInsiderComponent::SpawnInvitation_Implementation()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Insider SpawnInvitation"));
 	AHBHouse* TargetHouse = DetectHouse();
 	if (!TargetHouse) return;
-
-	UE_LOG(LogTemp, Warning, TEXT("Insider DetectHouse Done"));
+	
+	UE_LOG(LogTemp, Log, TEXT("House Detected"));
 	
 	FLinearColor HouseColor = TargetHouse->GetHouseColor();
 	if (InviteeColors.Contains(HouseColor)) return;
+
+	UE_LOG(LogTemp, Log, TEXT("Register Color"));
 	
 	InviteeColors.Add(HouseColor);
 
 	AHBCharacterPlayer* Insider = Cast<AHBCharacterPlayer>(GetOwner());
 	FVector SpawnLocation = Insider->GetActorLocation() + Insider->GetActorForwardVector() * 50.f;
 	FRotator SpawnRotation = Insider->GetActorRotation();
-	
+
+	UE_LOG(LogTemp, Log, TEXT("Spawn Invitation"));
 	AHBInvitation* Invitation = GetWorld()->SpawnActor<AHBInvitation>(InvitationClass, SpawnLocation, SpawnRotation);
 	Invitations.Add(Invitation);
 }
 
 void UHBJobInsiderComponent::GatherInvitees()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Insider GatherInvitees"));
 	if (!GetOwner()->HasAuthority()) return;
-
-	UE_LOG(LogTemp, Warning, TEXT("Insider GatherInvitees - 1"));
 
 	for (APlayerState* PlayerState : GetWorld()->GetGameState()->PlayerArray)
 	{
@@ -75,8 +73,8 @@ void UHBJobInsiderComponent::GatherInvitees()
 		if (InviteeColors.Contains(Character->PlayerColor))
 		{
 			float RandomTheta = FMath::DegreesToRadians(FMath::FRandRange(0.f, 360.f));
-			Character->SetActorLocation(GetOwner()->GetActorLocation() + FVector(FMath::Cos(RandomTheta), FMath::Sin(RandomTheta), 0.f) * 100);
-			UE_LOG(LogTemp, Warning, TEXT("Insider GatherInvitees - 2"));
+			Character->SetActorLocation(GetOwner()->GetActorLocation() + FVector(FMath::Cos(RandomTheta), FMath::Sin(RandomTheta), 0.f) * 50);
+			UE_LOG(LogTemp, Log, TEXT("Gather Invitee"));
 		}
 	}
 }
